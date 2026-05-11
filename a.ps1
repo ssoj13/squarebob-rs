79a94c6508a31f2955c93a2b7ddf1c0c9760f773
ssoj13 <96214515+ssoj13@users.noreply.github.com>
2026-05-11 01:18:40 -0700

    chore: WIP Mon 05/11/2026 -  1:18:39.84

--- a.ps1
+++
@@ -1,3 +0,0 @@
-﻿
-#$env:RUST_BACKTRACE=1; .\target\release\dirstat-rs.exe --log-modules pt 2>&1 | Tee-Object -FilePath profile.log
-.\target\release\dirstat-rs.exe --log-modules pt 2>&1 | Tee-Object profile2.log | Select-String "upload_scene|WF dispatch|cache MISS"
